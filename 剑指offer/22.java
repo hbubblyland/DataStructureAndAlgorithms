704837ea50414b89385bec6915a834e4c2da9f7f
hbubblyland <bubblyland@163.com>
2019-05-14 16:02:23 +0800

    22

---
+++ 剑指offer/22.java
@@ -0,0 +1,27 @@
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Solution {
+    public ListNode FindKthToTail(ListNode head,int k) {
+		if(head == null || k <= 0)
+			return null;
+		ListNode nodeA = head;
+		ListNode nodeB = head;
+		for(int i=0; i<k-1; i++){
+			if(nodeA.next != null)
+				nodeA = nodeA.next;
+			else
+				return null;
+		}
+		while(nodeA.next != null){
+			nodeA = nodeA.next;
+			nodeB = nodeB.next;
+		}
+    }
+}
