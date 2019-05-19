b9be13416ac241795781f58171a7d54b34cd8396
hbubblyland <bubblyland@163.com>
2019-05-19 19:32:06 +0800

    28

---
+++ 剑指offer/28.java
@@ -0,0 +1,51 @@
+/*
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+import java.util.Stack;
+public class Solution {
+    boolean isSymmetrical(TreeNode pRoot){
+		return isSymmetricalCore(pRoot,pRoot);
+	}
+	
+	boolean isSymmetricalCore(TreeNode pRoot1,TreeNode pRoot2){
+		if(pRoot1 == null && pRoot2 == null)
+			return true;
+		if(pRoot1 == null || pRoot2 == null)
+			return false;
+		if(pRoot1.val != pRoot2.val)
+			return false;
+		return isSymmetricalCore(pRoot1.left,pRoot2.right) && isSymmetricalCore(pRoot1.right,pRoot2.left);
+	}
+	
+	//非递归
+boolean isSymmetricalDFS(TreeNode pRoot)
+    {
+        if(pRoot == null) return true;
+        Stack<TreeNode> s = new Stack<>();
+        s.push(pRoot.left);
+        s.push(pRoot.right);
+        while(!s.empty()) {
+            TreeNode right = s.pop();//成对取出
+            TreeNode left = s.pop();
+            if(left == null && right == null) continue;
+            if(left == null || right == null) return false;
+            if(left.val != right.val) return false;
+            //成对插入
+            s.push(left.left);
+            s.push(right.right);
+            s.push(left.right);
+            s.push(right.left);
+        }
+        return true;
+    }
+}
